07453630e07fccd5e106a674578c49207b2290bc
Ali WaseeM <148793133+AliWaseem786@users.noreply.github.com>
2024-03-20 14:23:26 +0500

    cli

---
+++ app.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { message: "Enter Your FirstNumber", type: "number", name: "firstNumber" },
+    { message: "enter your second number", type: "number", name: "secondNumber" },
+    { message: "select one operator to operation", type: "list", name: "operator", choices: ["addition", "multiplication", "subtraction", "division"] }
+]);
+console.log(answer);
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("plz select valid operator");
+}
